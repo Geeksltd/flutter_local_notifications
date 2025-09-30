5ba0bb76a8529bc45911e93777e451bc76e51083
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2025-09-30 11:30:13 +0330

    Prevent crashes

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/ScheduledNotificationReceiver.java
@@ -25,41 +25,45 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
   @Override
   @SuppressWarnings("deprecation")
   public void onReceive(final Context context, Intent intent) {
-    String notificationDetailsJson =
-        intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
-    if (StringUtils.isNullOrEmpty(notificationDetailsJson)) {
-      // This logic is needed for apps that used the plugin prior to 0.3.4
+    try {
+      String notificationDetailsJson =
+          intent.getStringExtra(FlutterLocalNotificationsPlugin.NOTIFICATION_DETAILS);
+      if (StringUtils.isNullOrEmpty(notificationDetailsJson)) {
+        // This logic is needed for apps that used the plugin prior to 0.3.4
 
-      Notification notification;
-      int notificationId = intent.getIntExtra("notification_id", 0);
+        Notification notification;
+        int notificationId = intent.getIntExtra("notification_id", 0);
 
-      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
-        notification = intent.getParcelableExtra("notification", Notification.class);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+          notification = intent.getParcelableExtra("notification", Notification.class);
+        } else {
+          notification = intent.getParcelableExtra("notification");
+        }
+
+        if (notification == null) {
+          // This means the notification is corrupt
+          FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
+          Log.e(TAG, "Failed to parse a notification from  Intent. ID: " + notificationId);
+          return;
+        }
+
+        notification.when = System.currentTimeMillis();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(notificationId, notification);
+        boolean repeat = intent.getBooleanExtra("repeat", false);
+        if (!repeat) {
+          FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
+        }
       } else {
-        notification = intent.getParcelableExtra("notification");
-      }
+        Gson gson = FlutterLocalNotificationsPlugin.buildGson();
+        Type type = new TypeToken<NotificationDetails>() {}.getType();
+        NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
 
-      if (notification == null) {
-        // This means the notification is corrupt
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
-        Log.e(TAG, "Failed to parse a notification from  Intent. ID: " + notificationId);
-        return;
+        FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
+        FlutterLocalNotificationsPlugin.scheduleNextNotification(context, notificationDetails);
       }
-
-      notification.when = System.currentTimeMillis();
-      NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-      notificationManager.notify(notificationId, notification);
-      boolean repeat = intent.getBooleanExtra("repeat", false);
-      if (!repeat) {
-        FlutterLocalNotificationsPlugin.removeNotificationFromCache(context, notificationId);
-      }
-    } else {
-      Gson gson = FlutterLocalNotificationsPlugin.buildGson();
-      Type type = new TypeToken<NotificationDetails>() {}.getType();
-      NotificationDetails notificationDetails = gson.fromJson(notificationDetailsJson, type);
-
-      FlutterLocalNotificationsPlugin.showNotification(context, notificationDetails);
-      FlutterLocalNotificationsPlugin.scheduleNextNotification(context, notificationDetails);
+    } catch (Exception e) {
+      Log.e(TAG, "Error in onReceive: ", e);
     }
   }
 }
